bcc9910fd66a6e8ccfe3cfbbbd6d4e24faf5fdd5
Jean Hubert ABA'A <jhabaa@etu.he2b.be>
2023-10-30 19:07:58 +0100

    done!

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,34 @@
-// Get buttons
+// Get DOM
 const back_btn = document.querySelector("#back_btn")
 const fordward_btn = document.querySelector("#forward_btn")
-
-var container = document.querySelector(".container")
-
+let container = document.querySelector(".container")
 let cards = document.querySelectorAll(".card")
-var images_count = document.querySelectorAll(".card").length - 1;
-var indexes = [0,1,2] // Init with 23 firsts pictures that we want to display
-// Print the first 3 cards
+let images_count = document.querySelectorAll(".card").length - 1;
+let indexes = [0,1,2] // Init with 3 firsts pictures that we want to display
 
+//MARK: Cards are already hide in the css. We just need to show the first 3 cards
 indexes.forEach((index) => {
     cards[index].style.display = "flex";
 });
-
+//MARK: Let's create a function to update the buttons hide/show
+let updateButtons = () => {
+    if (indexes[0] == 0){
+        back_btn.style.display = "none";
+    }else{
+        back_btn.style.display = "flex";
+    }
+    if (indexes[indexes.length - 1] < images_count){
+        fordward_btn.style.display = "flex";
+    }else{
+        fordward_btn.style.display = "none";
+    }
+}
 
 back_btn.addEventListener("click", () => {  
     //MARK: Get images availables in the left
     let images_availables_on_left = indexes[0] > 3 ? 3 : indexes[0];
     //MARK: Create a new array with indexes move to the left by the images count in the left
     let new_indexes = indexes.map((index) => index - images_availables_on_left);
-    console.log("go to new_indexes: ", new_indexes)
     //MARK: Set indexes to True
     new_indexes.forEach((index) => {
         cards[index].style.display = "flex";
@@ -32,14 +41,8 @@ back_btn.addEventListener("click", () => {
     });
     //MARK: Update indexes
     indexes = new_indexes;
-    //MARK: Hide the left button if there is no more images to display
-    if (indexes[0] == 0){
-        back_btn.style.display = "none";
-    }
-    //MARK: Show the right button if there is more images to display
-    if (indexes[indexes.length - 1] < images_count){
-        fordward_btn.style.display = "flex";
-    }
+    //MARK: Update buttons
+    updateButtons()
 })
 fordward_btn.addEventListener("click", () => {
     //MARK: Get imges availables in the right
@@ -61,11 +64,5 @@ fordward_btn.addEventListener("click", () => {
     //MARK: Update indexes
     indexes = new_indexes;
     //MARK: Hide the right button if there is no more images to display
-    if (indexes[indexes.length - 1] == images_count){
-        fordward_btn.style.display = "none";
-    }
-    //MARK: Show the left button if there is more images to display
-    if (indexes[0] > 0){
-        back_btn.style.display = "flex";
-    }
+    updateButtons()
 }) 
